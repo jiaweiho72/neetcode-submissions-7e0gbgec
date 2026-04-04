22e9222a1d95ec8245e0f6da052ed9f436c97d87
Ho Jia Wei <99948407+jiaweiho72@users.noreply.github.com>
2026-04-04 21:19:03 +0800

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.py
@@ -0,0 +1,53 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        """
+        4 apr 2026
+        - output boolean
+        
+        idea 
+        - stack 
+            - for every close -> pop out the corresponding open
+                - check validity
+
+        - edge invalid cases
+            - more open than close
+            - more close than open
+
+        11mins
+        """
+
+        stack = []
+        for c in s:
+            if c in ["(", "{", "["]:
+                stack.append(c)
+            else:
+                if not stack: # no more open -> add this to prevent popping from empty stack
+                    return False
+                if c == ")":
+                    top = stack.pop()
+                    if top != "(":
+                        return False
+                elif c == "}":
+                    top = stack.pop()
+                    if top != "{":
+                        return False
+                elif c == "]":
+                    top = stack.pop()
+                    if top != "[":
+                        return False
+                else:
+                    return False
+        
+        # check against case where open>close -> stack has remaining elements
+        return not stack # True is stack is empty
+
+
+
+
+
+
+
+
+
+
+

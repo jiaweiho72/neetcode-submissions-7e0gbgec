2cca598803acb910ed8909ea3dbea67fcf35554b
Ho Jia Wei <99948407+jiaweiho72@users.noreply.github.com>
2026-04-05 00:16:07 +0800

    Add: find-target-in-rotated-sorted-array - submission-1

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-1.py
@@ -0,0 +1,45 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        """
+        4 apr 2026
+        - return index of target found in nums or -1 if not found
+        - all unique elements
+
+        - right region
+            - if m less than target -=> need larger
+                - if target more than the right side, need to search leftwards
+                - if smaller, search rightwards
+            - if m more than target -> need smaller
+                - just need to go leftwards
+
+        30mins
+
+        don't need to check if unrotated as the normal conditions would cover such cases
+        - from you target vs middle check
+        - getting min does not do that check
+        """
+
+        n = len(nums)
+        l, r = 0, n - 1
+        while l <= r:
+            m = (l + r) // 2
+            if nums[m] == target:
+                return m
+            
+            if nums[m] < nums[l]: # right region
+                # decide when to shift left or right
+                # if need smaller go left, if target bigger than the right, go left
+                if nums[m] > target or target > nums[r]:
+                    r = m - 1
+                else:
+                    l = m + 1
+                
+            else: # left region
+                # these below checks help to clear cases where unrotated
+                if nums[m] < target or target < nums[l]: # when to shift right
+                    l = m + 1
+                else:
+                    r = m - 1
+
+                    
+        return -1

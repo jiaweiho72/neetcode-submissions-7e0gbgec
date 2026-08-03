20d011165af74fc1b9d23d0c58826f914a1c299e
Ho Jia Wei <99948407+jiaweiho72@users.noreply.github.com>
2026-08-03 23:18:51 +0800

    Add: permutations - submission-1

---
+++ Data Structures & Algorithms/permutations/submission-1.py
@@ -0,0 +1,70 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        """ 
+        19 jun 2026
+        - return all -> backtracking
+
+        - not just simple choose or don't choose at each step
+            - each step you have multiple options to choose at each recursion as you can start from anywhere
+        
+        Time:
+        note O(n^n * n^2)
+            - as 
+        Space:
+        O(n) - recursion stack
+        """
+
+        result = []
+        cur_list = []
+        def backtracking(nums_left): # cur and the list left
+            # base case:
+            if not nums_left: # empty list
+                result.append(cur_list.copy())
+                return
+
+
+            for i in range(len(nums_left)):
+                num = nums_left[i]
+
+                # add current num
+                cur_list.append(num)
+                backtracking(nums_left[:i] + nums_left[i+1:])
+
+                # don't include (permutation you must include all elements, can't skip)
+                cur_list.pop()
+        
+        backtracking(nums)
+        return result
+
+
+
+        """
+        5 Apr
+        - return list of permutations
+
+        use and don't use
+
+        special
+        - need permutations of the remaining elements
+        """
+
+        result = []
+        n = len(nums)
+        visited = [False] * n  # track which numbers are used
+
+        def backtrack(cur):
+            if len(cur) == n:
+                result.append(cur.copy())
+                return
+
+            for i in range(n):
+                if visited[i]:
+                    continue
+                visited[i] = True
+                cur.append(nums[i])
+                backtrack(cur)
+                cur.pop()
+                visited[i] = False
+
+        backtrack([])
+        return result

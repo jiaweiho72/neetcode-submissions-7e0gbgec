4c97d2c8139a4db9c423557387feb19fe0f6268d
Ho Jia Wei <99948407+jiaweiho72@users.noreply.github.com>
2026-08-09 21:07:33 +0800

    Add: n-queens - submission-1

---
+++ Data Structures & Algorithms/n-queens/submission-1.py
@@ -0,0 +1,68 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        """
+        Question:
+        - Return a list of all possible n-queens board
+        - Queens travel vertically, horizontally and diagonally (any amount)
+        - Ensure no Queens clash AKA in the same col, row, diagonal
+
+        Idea: Brute-force backtracking
+        1) Maintain 3 sets
+            a) col -> Keep track of which column is used so that you don't add a queen in 
+            the same col
+            b) posDiag which similarly to ensure queens are not in the same diagonal
+            - Denoted by (r + c) as this is constant for every r,c pair along the diagonal
+            c) negDiag denoted by (r - c)
+        2) Just backtracking by the row and check for each col where it can put the queen
+
+
+        note: you could pass the sets as arguments but there is no need as it is a global thing reference
+
+        m is no of rows, n no of cols
+        Time: O(n!)
+        - recursion tree n^n
+            - depth/height = rows
+            - each branch factor = col
+            col ^ rows
+        - but once a col in col/diag set, it won't be processed again
+            so each dfs call, the branch factor decrease (n) * (n-1) * (n-2) ... = n!
+        
+        Space O(n^2 * no_of_solutions)
+        sets -> n
+        each board n^2
+        """
+
+        result = []
+        col_set = set()
+        pos_diag_set = set() # r + c /
+        neg_diag_set = set() # r - c \
+        board = [["."] * n for _ in range(n)] # Initialise board
+
+        # Going through each row so there is no clash
+        def backtracking(row):
+            if row == n: # reached the end
+                copy = [''.join(row) for row in board]
+                result.append(copy)
+                return
+            
+            for col in range(n):
+                # If current row, col is invalid as there is a conflicting queen
+                if col in col_set or (row + col) in pos_diag_set or (row - col) in neg_diag_set:
+                    continue # This goes to the next iteration of the loop
+                
+                # Try add in row, col
+                col_set.add(col)
+                pos_diag_set.add(row + col)
+                neg_diag_set.add(row - col)
+                board[row][col] = "Q"
+
+                backtracking(row + 1)
+
+                # Backtrack and try another column
+                col_set.remove(col)
+                pos_diag_set.remove(row + col)
+                neg_diag_set.remove(row - col)
+                board[row][col] = "."
+        
+        backtracking(0)
+        return result

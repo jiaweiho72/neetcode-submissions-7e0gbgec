757539da834ac2293fcd284b61c8603a1917bd78
Ho Jia Wei <99948407+jiaweiho72@users.noreply.github.com>
2026-08-23 13:44:07 +0800

    Add: remove-duplicates-from-sorted-array - submission-0

---
+++ Data Structures & Algorithms/remove-duplicates-from-sorted-array/submission-0.py
@@ -0,0 +1,21 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        """
+        sorted. Remove duplicates inplace. return no of unique elements such that first k values are the unique values
+
+        the back, don't need to consider
+
+        idea
+        - two pointer, write position and the next position
+        """
+        n = len(nums)
+        write, read = 0, 0
+
+        while read < n:
+            while read + 1 < n and nums[read] == nums[read + 1]:
+                read += 1
+            nums[write] = nums[read]
+            read += 1
+
+            write += 1
+        return write

a83750efa144241c364e6677cbfd26f5e1d61199
Ho Jia Wei <99948407+jiaweiho72@users.noreply.github.com>
2026-08-23 23:48:26 +0800

    Add: minimum-recolors-to-get-k-consecutive-black-blocks - submission-1

---
+++ Data Structures & Algorithms/minimum-recolors-to-get-k-consecutive-black-blocks/submission-1.py
@@ -0,0 +1,36 @@
+class Solution:
+    def minimumRecolors(self, blocks: str, k: int) -> int:
+        """
+        - return min no of operations (convert white -> black) to get at least one
+        occurrence of k consecutive black blocks
+
+        Idea - consecutive (sliding window)
+        - maintain window of size k
+        - find the subarray with the max count of black. 
+        - k - max_count_black_in_subarray = no of operations
+
+        Time: O(n) one pass sliding window
+        Space: O(1) no list, just managing black_count variable
+
+        12:58mins
+        """
+        n = len(blocks)
+
+        # 1) init window
+        black_count = 0 # sliding window
+        for i in range(k):
+            if blocks[i] == "B":
+                black_count += 1
+ 
+        max_black_count = black_count
+        # 2) main sliding window
+        for i in range(k, n): # k - 1 + 1
+            if blocks[i] == "B": # Add new
+                black_count += 1
+            if blocks[i - k] == "B": # Remove back
+                black_count -= 1
+            max_black_count = max(max_black_count, black_count)
+
+        return k - max_black_count
+
+

3b0b9c67fd100d1cc8539239d83de416b3e40267
Ho Jia Wei <99948407+jiaweiho72@users.noreply.github.com>
2026-08-25 00:04:44 +0800

    Add: minimum-size-subarray-sum - submission-1

---
+++ Data Structures & Algorithms/minimum-size-subarray-sum/submission-1.py
@@ -0,0 +1,33 @@
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        """
+        24 Aug 2026
+        - return min length of subarray whose sum >= target
+        - return 0 if no such subarray
+        note: positive numbers
+
+        idea
+        - contiguous subarray of fixed condition (sum <= target) -> sliding window
+        - constantly check for the min_length
+        - this sliding window does not need to init as the size is not fixed. condition is not the size but the sum property
+        
+        Time: O(2n)
+        Space: O(1)
+        9:33 mins
+
+        """
+
+        n = len(nums)
+        l = 0
+        min_length = float('inf')
+        cur_sum = 0
+
+        for r in range(n):
+            cur_sum += nums[r]
+            while cur_sum >= target: # while still valid -> decrement the left side
+                min_length = min(min_length, r - l + 1) # it has been checked that cur_sum >= target
+                
+                cur_sum -= nums[l]
+                l += 1
+
+        return min_length if min_length != float('inf') else 0

25ba4064ce2b99eb89d00aabb456b35aee3d5f4a
Ho Jia Wei <99948407+jiaweiho72@users.noreply.github.com>
2026-08-26 00:33:16 +0800

    Add: online-stock-span - submission-0

---
+++ Data Structures & Algorithms/online-stock-span/submission-0.py
@@ -0,0 +1,39 @@
+class StockSpanner:
+    """
+    26 Aug 2026
+    question
+    - insert price and return span
+    - span is the no of consecutive days before (including today)
+        where price < today
+    
+    bruteforce
+    - every next
+        - go through every element to check if today greater than each prev price O(n)
+    
+    optimal idea - monotonic decreasing (maintain decreasing)
+    - if meet an increasing today high price, pop from stack until today is lower price
+    - need a way to optimally get the span without searching every previous element
+    - stack stores (price, span)
+        - after popping, the span is reflects and you will sum the spans
+    
+    <=
+    """
+
+    def __init__(self):
+        self.stack = [] # monotonic decreasing (price, span)
+
+    def next(self, price: int) -> int:
+        cur_span = 1 # include itself
+        while self.stack and self.stack[-1][0] <= price: # while today price is more/equal than previous days
+            prev_price, prev_span = self.stack.pop()
+            cur_span += prev_span
+        self.stack.append((price, cur_span))
+        return cur_span
+
+
+        
+
+
+# Your StockSpanner object will be instantiated and called as such:
+# obj = StockSpanner()
+# param_1 = obj.next(price)

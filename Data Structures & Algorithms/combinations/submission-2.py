1ca0aac97770d5fc0ca90cbed4d504b6b54ed2d5
Ho Jia Wei <99948407+jiaweiho72@users.noreply.github.com>
2026-09-11 00:48:39 +0800

    Add: combinations - submission-2

---
+++ Data Structures & Algorithms/combinations/submission-2.py
@@ -0,0 +1,59 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        """
+        11 Sep 2026
+        - return all possible combinations of k numbers chosen from 1-n
+
+        go through every 1-n
+        - inclusive of n
+        - instead of returning all sizes like in combination
+            - add to result list only when size == k
+        - input no duplicate so no need to handle and skip
+
+
+        idea of forloop
+        - once you choose this element j, you never choose it again
+            - so the search space decreases.
+            - don't include it or the previous elements
+                - previous one is handled by previous elements
+            like you choose j for current
+            - there won't be another exact case where the front part is the same and it choose j again, not possible
+                    1,2,3
+                    / | \
+                   1  2  3
+                  /   |
+                 2    1
+            - don't go back to previous elements because choosing it will cause a 1,2 and 2,1
+                - if select 1, let that pass handle ALL the combinations with 1
+                    - other paths should not contain 1
+                - branching this out, previous elements won't 
+                - basically if choose current element, it will handle all combinations after it. 
+                    other paths should not tou
+            - go forward and not backtrack or you will reprocess your parent
+        time: O(2^n) 
+        space: O(2^n) stack
+        10mins
+        """
+        result = []
+        def dfs(i, cur):
+            # 1) base case
+            if len(cur) == k:
+                result.append(cur.copy())
+                return
+
+            # 2) main loop
+            for j in range(i, n + 1): # j includes i too
+                # NA - violates constraint
+
+                # add current j element
+                cur.append(j)
+                dfs(j + 1, cur)
+                cur.pop()
+
+        dfs(1, [])
+        return result
+
+
+
+
+
